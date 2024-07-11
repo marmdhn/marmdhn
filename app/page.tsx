dbbc17d709fcaac627d8d8fb551df7543f6b879e
Ramadhan <makbarramadhan62@gmail.com>
2024-07-11 14:13:19 +0700

    refactor: implement layout & animated components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import AnimatedGrid from "@/components/AnimatedGrid";
+import AnimatedText from "@/components/AnimatedText";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-4">
+      <div className="z-10 w-full max-w-7xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+          My Portfolio
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -17,10 +18,10 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src="/mar_logo.svg"
               alt="Vercel Logo"
               className="dark:invert"
-              width={100}
+              width={75}
               height={24}
               priority
             />
@@ -28,85 +29,68 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+      <div className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="col-span-1 row-span-2 flex flex-col items-center justify-center bg-gray-800 p-4 rounded-lg">
+          <div className="w-24 h-24 rounded-full overflow-hidden bg-gray-600 mb-4">
+            <Image
+              src={"/profile.png"}
+              alt={"image"}
+              width={100}
+              height={100}
+              className="object-contain w-full h-full"
+            />
+          </div>
+          <span className="text-white">Muhammad Akbar Ramadhan</span>
+        </div>
+        <div className="col-span-1 bg-gray-800 p-4 rounded-lg">
+          <span className="text-white">GI Assets</span>
+          <p className="text-gray-400">
+            Character textures, models, and mods for a certain anime game
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <div className="flex mt-2">
+            <span className="text-white mr-2">1.6k</span>
+            <span className="text-white">1.0k</span>
+          </div>
+        </div>
+        <div className="col-span-1 bg-gray-800 p-4 rounded-lg">
+          <span className="text-white">Wallhelper</span>
+          <p className="text-gray-400">
+            Some scripts to use video wallpapers on linux
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="flex mt-2">
+            <span className="text-white mr-2">17</span>
+            <span className="text-white">3</span>
+          </div>
+        </div>
+        <div className="col-span-1 row-span-3 bg-gray-800 p-4 rounded-lg">
+          <AnimatedGrid />
+        </div>
+        <div className="col-span-2 row-span-3 bg-gray-800 p-4 rounded-lg flex items-center justify-center">
+          <AnimatedText />
+        </div>
+        <div className="col-span-1 bg-gray-800 p-4 rounded-lg flex items-center justify-center">
+          <span className="text-white">
+            I&apos;m not doing anything right now...
+          </span>
+        </div>
+        <div className="col-span-1 bg-gray-800 p-4 rounded-lg flex items-center justify-center">
+          <span className="text-white">Wow! What a cool site</span>
+        </div>{" "}
+        <div className="col-span-1 bg-gray-800 p-4 rounded-lg flex items-center justify-center">
+          <span className="text-white">Wow! What a cool site</span>
+        </div>
+        <div className="col-span-4 bg-gray-800 p-4 rounded-lg flex justify-between">
+          <div className="flex space-x-2">
+            <div className="w-6 h-6 bg-gray-600"></div>
+            <div className="w-6 h-6 bg-gray-600"></div>
+            <div className="w-6 h-6 bg-gray-600"></div>
+          </div>
+          <div className="flex space-x-2">
+            <div className="w-6 h-6 bg-gray-600"></div>
+            <div className="w-6 h-6 bg-gray-600"></div>
+            <div className="w-6 h-6 bg-gray-600"></div>
+          </div>
+        </div>
       </div>
     </main>
   );
