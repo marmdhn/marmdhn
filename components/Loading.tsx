2628695ac16780301646c09fd19cc8f9f680be1c
Ramadhan <makbarramadhan62@gmail.com>
2024-07-13 21:10:35 +0700

    feat: add loading

---
+++ components/Loading.tsx
@@ -0,0 +1,10 @@
+// components/Loading.js
+const Loading = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-gray-900">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900 dark:border-white"></div>
+    </div>
+  );
+};
+
+export default Loading;
