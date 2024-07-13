9c2a9c3f887d54a3cd5f207b3c8702ac59e948aa
Ramadhan <makbarramadhan62@gmail.com>
2024-07-13 22:38:37 +0700

    style: adjust colot

--- components/AnimatedLogo.tsx
+++ components/AnimatedLogo.tsx
@@ -30,10 +30,10 @@ const AnimatedLogo = () => {
     >
       <defs>
         <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop offset="0%" style={{ stopColor: "#74EBD5", stopOpacity: 1 }} />
+          <stop offset="0%" style={{ stopColor: "#27BBA0", stopOpacity: 1 }} />
           <stop
             offset="100%"
-            style={{ stopColor: "#ACB6E5", stopOpacity: 1 }}
+            style={{ stopColor: "#6A7CD3", stopOpacity: 1 }}
           />
         </linearGradient>
       </defs>
