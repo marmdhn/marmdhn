80343a9ba289339ce37168ba15a385dd963698be
Ramadhan <makbarramadhan62@gmail.com>
2024-07-13 22:08:07 +0700

    refactor: remove description

--- data/portfolios.ts
+++ data/portfolios.ts
@@ -3,20 +3,19 @@ import { PortfolioCardTypes } from "@/types/PortfolioCardTypes";
 export const portfolios: PortfolioCardTypes[] = [
   {
     title: "Website Sekolah Baitul Adab",
-    description:
-      "Website Sekolah Baitul Adab menyediakan informasi lengkap tentang sekolah, termasuk program akademik, berita, dan kegiatan. Dengan desain yang modern dan mudah digunakan, website ini memudahkan siswa, orang tua, dan masyarakat untuk mendapatkan informasi terkini.",
+
     techStack: ["Next.js", "Tailwind CSS"],
-    companyName: "Baitul Adab",
+    companyName: "Baitul Adab Islamic School",
     companyLogo: "baituladab.svg",
     type: "website",
     images: ["imageTest.jpg", "imageNotFound.png"],
+    imageUrl: "baitul-adab.png",
     githubRepo: "https://github.com/makbarramadhan62/baitul-adab-react",
     webUrl: "https://baitul-adab.vercel.app/",
   },
   {
     title: "Dashboard Monitoring Server",
-    description:
-      "Dashboard untuk memonitor performa server di PT. Akhdani Reka Solusi",
+
     techStack: ["React", "Redux", "Chart.js"],
     companyName: "PT. Akhdani Reka Solusi",
     companyLogo: "ars.svg",
