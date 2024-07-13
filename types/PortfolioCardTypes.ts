ff1c1fe59bef89d8f5a92fbc226e8ca93964514b
Ramadhan <makbarramadhan62@gmail.com>
2024-07-13 21:09:56 +0700

    feat: add Portfolio Card Types

---
+++ types/PortfolioCardTypes.ts
@@ -0,0 +1,12 @@
+export interface PortfolioCardTypes {
+  title: string;
+  description: string;
+  techStack: string[];
+  imageUrl?: string;
+  companyName: string;
+  companyLogo: string;
+  type: string;
+  images: string[];
+  githubRepo?: string;
+  webUrl?: string;
+}
