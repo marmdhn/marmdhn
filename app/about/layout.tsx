1c8e4a84f8e27c4dac07ef7f80f3e8a8425ff069
Ramadhan <makbarramadhan62@gmail.com>
2024-07-13 22:23:16 +0700

    feat: add about page

---
+++ app/about/layout.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+import { Inter } from "next/font/google";
+import ClientSideLayout from "@/components/ClientSideLayout";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+interface ProjectsLayoutProps {
+  children: ReactNode;
+}
+
+export const metadata = {
+  title: "MARMDHN | About",
+  description: "This is the projects page of MARMDHN_ portfolio.",
+};
+
+export default function AboutLayout({ children }: ProjectsLayoutProps) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <ClientSideLayout>{children}</ClientSideLayout>
+      </main>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import PortfolioSection from "@/app/projects/PortfolioSection";
+
+const About = () => {
+  return (
+    <div className="max-w-7xl px-8 xl:px-0 mx-auto py-10 h-full">
+      <div className="text-center mb-8">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-4xl font-bold text-primary mt-4">About Me</h1>
+          <p className="text-gray-700 dark:text-gray-300 text-lg font-semibold">
+            Comming Soon.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
