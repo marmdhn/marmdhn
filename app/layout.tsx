e0cb9b38561bbb851790e7ae5184632b756fd98f
Ramadhan <makbarramadhan62@gmail.com>
2024-07-14 10:55:15 +0700

    refactor: implement my theme provide

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientSideLayout from "@/components/ClientSideLayout";
+import MyThemeProvider from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} h-full`}>
-        <ClientSideLayout>{children}</ClientSideLayout>
+        <MyThemeProvider>
+          <ClientSideLayout>{children}</ClientSideLayout>
+        </MyThemeProvider>
       </body>
     </html>
   );
