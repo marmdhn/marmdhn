8f77da2ed2b9f6a4c16f520b91bba76ad4676f04
Ramadhan <makbarramadhan62@gmail.com>
2024-07-14 17:21:06 +0700

    refactor: implement for active path

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import Link from "next/link";
 import ToggleDarkMode from "@/components/ToggleDarkMode";
 import { FaGithub } from "react-icons/fa6";
 import Image from "next/image";
 import NavbarMobile from "@/components/NavbarMobile";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  const [activePath, setActivePath] = useState("");
+  useEffect(() => {
+    setActivePath(pathname);
+    console.log(pathname);
+  }, [pathname]);
+
   return (
     <nav className="px-8 sm:px-0 backdrop-blur-2xl dark:bg-transparent from-inherit bg-white transition-all duration-200 shadow-lg sticky top-0">
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
@@ -36,20 +47,32 @@ const Navbar = () => {
               <div className="space-x-4 justify-center hidden sm:flex">
                 <Link
                   href="/"
-                  className="text-gray-900 px-3 py-2 rounded-md text-lg font-semibold hover:text-primary dark:hover:text-primary dark:text-white transition-all duration-200"
+                  className={`text-gray-900 px-3 py-2 rounded-md text-lg font-semibold ${
+                    activePath === "/"
+                      ? "text-primary dark:text-primary"
+                      : "hover:text-primary dark:hover:text-primary"
+                  } dark:text-white transition-all duration-200`}
                 >
                   Home
                 </Link>
 
                 <Link
                   href="/projects"
-                  className="text-gray-900 px-3 py-2 rounded-md text-lg font-semibold hover:text-primary dark:hover:text-primary dark:text-white transition-all duration-200"
+                  className={`text-gray-900 px-3 py-2 rounded-md text-lg font-semibold ${
+                    activePath === "/projects"
+                      ? "text-primary dark:text-primary"
+                      : "hover:text-primary dark:hover:text-primary dark:text-white"
+                  } transition-all duration-200`}
                 >
                   Projects
                 </Link>
                 <Link
                   href="/about"
-                  className="text-gray-900 px-3 py-2 rounded-md text-lg font-semibold hover:text-primary dark:hover:text-primary dark:text-white transition-all duration-200"
+                  className={`text-gray-900 px-3 py-2 rounded-md text-lg font-semibold ${
+                    activePath === "/about"
+                      ? "text-primary dark:text-primary"
+                      : "hover:text-primary dark:hover:text-primary dark:text-white"
+                  } transition-all duration-200`}
                 >
                   About
                 </Link>
