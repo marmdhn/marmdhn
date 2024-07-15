98bb0cb58d7925959a50a1ed42cd87b84cf4cd2a
Ramadhan <makbarramadhan62@gmail.com>
2024-07-15 09:20:36 +0700

    refactor: implement loading ui while load image

--- components/CardPortfolio.tsx
+++ components/CardPortfolio.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { PortfolioCardTypes } from "@/types/PortfolioCardTypes";
 import ModalPortfolio from "@/components/ModalPortfolio";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import Link from "next/link";
+import { FaImage } from "react-icons/fa6";
 
 const CardPortfolio: React.FC<PortfolioCardTypes> = ({
   title,
@@ -27,18 +29,36 @@ const CardPortfolio: React.FC<PortfolioCardTypes> = ({
     webUrl,
   };
 
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <div className="bg-white dark:bg-gray-800 transition-all duration-200 shadow-lg rounded-lg overflow-hidden">
-      <Image
-        unoptimized
-        width={100}
-        height={100}
-        className="w-full h-48 object-cover object-center"
-        src={`/images/portfolio/${
-          imageUrl !== undefined ? imageUrl : "imageNotFound.png"
+    <div className="w-full bg-white dark:bg-gray-800 transition-all duration-200 shadow-lg rounded-lg overflow-hidden">
+      <div
+        className={`relative flex items-center justify-center max-w-sm transition-all duration-200 ${
+          isLoading
+            ? "bg-gray-300 animate-pulse dark:bg-gray-700 h-56 w-full"
+            : ""
         }`}
-        alt={title}
-      />
+      >
+        {isLoading && (
+          <FaImage className="absolute w-10 h-10 text-gray-200 dark:text-gray-600 transition-all duration-200" />
+        )}
+        <Image
+          key={imageUrl}
+          unoptimized
+          width={100}
+          height={100}
+          className={`w-full h-56 object-cover object-center transition-opacity duration-500 ${
+            isLoading ? "opacity-0" : "opacity-100"
+          }`}
+          src={`/images/portfolio/${
+            imageUrl !== undefined ? imageUrl : "imageNotFound.png"
+          }`}
+          alt={title}
+          onLoad={() => setTimeout(() => setIsLoading(false), 500)}
+        />
+      </div>
+
       <div className="p-4">
         <div className="flex justify-between items-center">
           <h2 className="text-xl font-bold text-gray-900 dark:text-white">
@@ -59,27 +79,27 @@ const CardPortfolio: React.FC<PortfolioCardTypes> = ({
 
         <div className="flex justify-start items-center gap-4">
           {githubRepo ? (
-            <a
+            <Link
               href={githubRepo}
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-600 hover:underline"
             >
               GitHub Repo <FaExternalLinkAlt className="inline ml-1" />
-            </a>
+            </Link>
           ) : (
             <span className="text-gray-400">GitHub Repo not available</span>
           )}
 
           {webUrl ? (
-            <a
+            <Link
               href={webUrl}
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-600 hover:underline"
             >
               Website URL <FaExternalLinkAlt className="inline ml-1" />
-            </a>
+            </Link>
           ) : (
             <span className="text-gray-400">Website URL not available</span>
           )}
