94dc5f9d0807f96bed8ec635a626e23099fff0f8
Ramadhan <makbarramadhan62@gmail.com>
2024-07-15 14:59:59 +0700

    refactor: implement animation using framer motion

--- components/CardPortfolio.tsx
+++ components/CardPortfolio.tsx
@@ -9,9 +9,8 @@ import { FaImage } from "react-icons/fa6";
 const CardPortfolio: React.FC<PortfolioCardTypes> = ({
   title,
   techStack,
-  imageUrl,
+  imageCover,
   companyName,
-  companyLogo,
   type,
   images,
   githubRepo,
@@ -20,9 +19,8 @@ const CardPortfolio: React.FC<PortfolioCardTypes> = ({
   const portfolioModalProps = {
     title,
     techStack,
-    imageUrl,
+    imageCover,
     companyName,
-    companyLogo,
     type,
     images,
     githubRepo,
@@ -32,9 +30,9 @@ const CardPortfolio: React.FC<PortfolioCardTypes> = ({
   const [isLoading, setIsLoading] = useState(true);
 
   return (
-    <div className="w-full bg-white dark:bg-gray-800 transition-all duration-200 shadow-lg rounded-lg overflow-hidden">
+    <div className="w-96 bg-white dark:bg-gray-800 transition-all duration-200 shadow-lg rounded-lg overflow-hidden flex flex-col justify-between">
       <div
-        className={`relative flex items-center justify-center max-w-sm transition-all duration-200 ${
+        className={`relative flex items-center justify-center transition-all duration-200 ${
           isLoading
             ? "bg-gray-300 animate-pulse dark:bg-gray-700 h-56 w-full"
             : ""
@@ -44,7 +42,7 @@ const CardPortfolio: React.FC<PortfolioCardTypes> = ({
           <FaImage className="absolute w-10 h-10 text-gray-200 dark:text-gray-600 transition-all duration-200" />
         )}
         <Image
-          key={imageUrl}
+          key={imageCover}
           unoptimized
           width={100}
           height={100}
@@ -52,61 +50,65 @@ const CardPortfolio: React.FC<PortfolioCardTypes> = ({
             isLoading ? "opacity-0" : "opacity-100"
           }`}
           src={`/images/portfolio/${
-            imageUrl !== undefined ? imageUrl : "imageNotFound.png"
+            imageCover !== undefined ? imageCover : "imageNotFound.png"
           }`}
           alt={title}
           onLoad={() => setTimeout(() => setIsLoading(false), 500)}
         />
       </div>
 
-      <div className="p-4">
-        <div className="flex justify-between items-center">
-          <h2 className="text-xl font-bold text-gray-900 dark:text-white">
-            {title}
-          </h2>
-        </div>
-        <p className="text-gray-400 mb-4">{companyName}</p>
-        <div className="flex flex-wrap gap-2 mb-4">
-          {techStack.map((tech, index) => (
-            <span
-              key={index}
-              className="inline-block bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-300 px-2 py-1 text-xs font-semibold rounded-md"
-            >
-              {tech}
-            </span>
-          ))}
-        </div>
-
-        <div className="flex justify-start items-center gap-4">
-          {githubRepo ? (
-            <Link
-              href={githubRepo}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 hover:underline"
-            >
-              GitHub Repo <FaExternalLinkAlt className="inline ml-1" />
-            </Link>
-          ) : (
-            <span className="text-gray-400">GitHub Repo not available</span>
-          )}
-
-          {webUrl ? (
-            <Link
-              href={webUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 hover:underline"
-            >
-              Website URL <FaExternalLinkAlt className="inline ml-1" />
-            </Link>
-          ) : (
-            <span className="text-gray-400">Website URL not available</span>
-          )}
+      <div className="p-4 flex flex-col flex-grow justify-between">
+        <div className="mb-4">
+          <div className="flex justify-between items-center mb-2">
+            <h2 className="text-xl font-bold text-gray-900 dark:text-white">
+              {title}
+            </h2>
+          </div>
+          <p className="text-gray-400 mb-4">{companyName}</p>
+          <div className="flex flex-wrap gap-2 mb-4">
+            {techStack.map((tech, index) => (
+              <span
+                key={index}
+                className="inline-block bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-300 px-2 py-1 text-xs font-semibold rounded-md"
+              >
+                {tech}
+              </span>
+            ))}
+          </div>
         </div>
 
-        <div className="w-full flex justify-start mt-4">
-          <ModalPortfolio portfolio={portfolioModalProps} />
+        <div className="mt-auto">
+          <div className="flex justify-start items-center gap-4 mb-4">
+            {githubRepo ? (
+              <Link
+                href={githubRepo}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 hover:underline"
+              >
+                GitHub Repo <FaExternalLinkAlt className="inline ml-1" />
+              </Link>
+            ) : (
+              <span className="text-gray-400">GitHub Repo not available</span>
+            )}
+
+            {webUrl ? (
+              <Link
+                href={webUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 hover:underline"
+              >
+                Website URL <FaExternalLinkAlt className="inline ml-1" />
+              </Link>
+            ) : (
+              <span className="text-gray-400">Website URL not available</span>
+            )}
+          </div>
+
+          <div className="w-full flex justify-start">
+            <ModalPortfolio portfolio={portfolioModalProps} />
+          </div>
         </div>
       </div>
     </div>
