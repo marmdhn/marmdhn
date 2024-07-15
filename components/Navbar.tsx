f4c99ef9559bf020bdd694daef0c6fc2816343bb
Ramadhan <makbarramadhan62@gmail.com>
2024-07-15 15:07:29 +0700

    style: adjust shadow color in dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   }, [pathname]);
 
   return (
-    <nav className="px-8 sm:px-0 backdrop-blur-2xl dark:bg-transparent from-inherit bg-white transition-all duration-200 shadow-lg sticky top-0 z-50 overflow-hidden">
+    <nav className="px-8 sm:px-0 backdrop-blur-2xl dark:bg-transparent from-inherit bg-white transition-all duration-200 shadow-lg dark:shadow-gray-900 sticky top-0 z-50 overflow-hidden">
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="grid grid-cols-3 items-center justify-items-center w-full">
