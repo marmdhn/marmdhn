87dd81deaf10c7df937a2dfe2de777ce9bbcc88a
Ramadhan <makbarramadhan62@gmail.com>
2024-07-16 19:38:19 +0700

    feat: add speed insight from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientSideLayout from "@/components/ClientSideLayout";
 import MyThemeProvider from "@/components/ThemeProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} h-full`}>
         <MyThemeProvider>
-          <ClientSideLayout>{children}</ClientSideLayout>
+          <ClientSideLayout>
+            {children}
+            <SpeedInsights />
+          </ClientSideLayout>
         </MyThemeProvider>
       </body>
     </html>
