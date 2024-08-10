7c54e1764f98c377bb0bb2bf130bbece27b6975f
Ramadhan <makbarramadhan62@gmail.com>
2024-08-10 18:09:43 +0700

    feat: add games layout

---
+++ app/games/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+interface ProjectsLayoutProps {
+  children: ReactNode;
+}
+
+export const metadata = {
+  title: "MARMDHN | Games",
+  description: "This is the projects page of MARMDHN_ portfolio.",
+};
+
+export default function AboutLayout({ children }: ProjectsLayoutProps) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
