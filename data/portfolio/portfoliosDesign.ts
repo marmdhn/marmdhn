efc76b7ea593b039f5a2291ecb6f45ff214d8300
Ramadhan <makbarramadhan62@gmail.com>
2024-08-11 20:12:57 +0700

    feat: add data for design

---
+++ data/portfolio/portfoliosDesign.ts
@@ -0,0 +1,16 @@
+import { PortfolioCardTypes } from "@/types/PortfolioCardTypes";
+
+export const portfoliosDesign: PortfolioCardTypes[] = [
+  // {
+  //   title: "MARental",
+  //   imageCover: "MARental.png",
+  //   techStack: ["Figma"],
+  //   companyName: "Personal Project",
+  //   type: "design",
+  //   images: [
+  //     "/MARental/MARental-1.png",
+  //     "/MARental/MARental-2.png",
+  //     "/MARental/MARental-3.png",
+  //   ],
+  // },
+];
