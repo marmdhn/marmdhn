d595ae0c2668a6a2caf1b494b531fbe7bd36a579
Ramadhan <makbarramadhan62@gmail.com>
2024-08-11 20:13:05 +0700

    feat: add data for UIUX

---
+++ data/portfolio/portfoliosUIUX.ts
@@ -0,0 +1,40 @@
+import { PortfolioCardTypes } from "@/types/PortfolioCardTypes";
+
+export const portfoliosUIUX: PortfolioCardTypes[] = [
+  {
+    title: "MARental",
+    imageCover: "MARental.png",
+    techStack: ["Figma"],
+    companyName: "Personal Project",
+    type: "ui/ux",
+    images: [
+      "/MARental/MARental-1.png",
+      "/MARental/MARental-2.png",
+      "/MARental/MARental-3.png",
+    ],
+  },
+  {
+    title: "MAR Anime List",
+    imageCover: "mar-anime-list.png",
+    techStack: ["Figma"],
+    companyName: "Personal Project",
+    type: "ui/ux",
+    images: [
+      "/mar-anime-list/mar-anime-list-1.png",
+      "/mar-anime-list/mar-anime-list-2.png",
+      "/mar-anime-list/mar-anime-list-3.png",
+    ],
+  },
+  {
+    title: "User Management",
+    imageCover: "user-management.png",
+    techStack: ["Figma"],
+    companyName: "Personal Project",
+    type: "ui/ux",
+    images: [
+      "/user-management/user-management-1.png",
+      "/user-management/user-management-2.png",
+      "/user-management/user-management-3.png",
+    ],
+  },
+];
