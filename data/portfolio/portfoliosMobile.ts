69e61e4751413b447e795d3d41f9a0ecc66a9803
Ramadhan <makbarramadhan62@gmail.com>
2024-08-11 20:13:01 +0700

    feat: add data for mobile

---
+++ data/portfolio/portfoliosMobile.ts
@@ -0,0 +1,52 @@
+import { PortfolioCardTypes } from "@/types/PortfolioCardTypes";
+
+export const portfoliosMobile: PortfolioCardTypes[] = [
+  {
+    title: "MAR Quran App",
+    imageCover: "mar-quran.png",
+    techStack: ["Flutter"],
+    companyName: "Personal Project",
+    type: "mobile",
+    images: [
+      "/mar-quran/mar-quran-1.png",
+      "/mar-quran/mar-quran-2.png",
+      "/mar-quran/mar-quran-3.png",
+    ],
+    webUrl:
+      "https://drive.google.com/file/d/1Dwc0zNCEe3rV5m9_r6JVX-LXHD48a1_F/view?usp=sharing",
+    githubRepo: "https://github.com/marmdhn/mar-quran-app/",
+  },
+  {
+    title: "Grapesense",
+    imageCover: "grapesense.png",
+    techStack: [
+      "Flutter",
+      "Python",
+      "Machine Learning",
+      "Naive Bayes Algorithm",
+      "Flask API",
+    ],
+    companyName: "Final Project",
+    type: "mobile",
+    images: [
+      "/grapesense/grapesense-1.png",
+      "/grapesense/grapesense-2.png",
+      "/grapesense/grapesense-3.png",
+      "/grapesense/grapesense-4.png",
+    ],
+    githubRepo: "https://github.com/marmdhn/skripsi-grapesense",
+  },
+  {
+    title: "OK Docs",
+    imageCover: "ok-docs.png",
+    techStack: ["Flutter", "Laravel", "phpMyAdmin", "MySQL"],
+    companyName: "PT Otak Kanan",
+    type: "mobile",
+    images: [
+      "/ok-docs/ok-docs-1.png",
+      "/ok-docs/ok-docs-2.png",
+      "/ok-docs/ok-docs-3.png",
+    ],
+    githubRepo: "https://github.com/marmdhn/ok-docs-managemant-apps",
+  },
+];
