04938e09874c392bd420c142290eed4fbac8da28
Ramadhan <makbarramadhan62@gmail.com>
2024-08-11 20:43:11 +0700

    feat: add gallery page

---
+++ app/gallery/page.tsx
@@ -0,0 +1,18 @@
+const Gallery = () => {
+  return (
+    <div className="max-w-7xl px-8 xl:px-0 mx-auto py-10 h-full">
+      <div className="text-center mb-8">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-4xl font-bold text-primary mt-4">
+            Gallery Section
+          </h1>
+          <p className="text-gray-700 dark:text-gray-300 text-lg font-semibold">
+            marmdhn.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
