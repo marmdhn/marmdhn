96fd87a0463dc732c31397f5c1811c2c1545b755
Ramadhan <makbarramadhan62@gmail.com>
2024-08-11 20:13:09 +0700

    feat: add data for video

---
+++ data/portfolio/portfoliosVIdeo.ts
@@ -0,0 +1,12 @@
+import { PortfolioCardTypes } from "@/types/PortfolioCardTypes";
+
+export const portfoliosVideo: PortfolioCardTypes[] = [
+  {
+    title: "MAR Logo Reveal",
+    imageCover: "thumbnail_1.png",
+    techStack: ["Adobe After Effects", "Adobe Premiere Pro"],
+    companyName: "Personal Project",
+    type: "Video",
+    videoUrl: "https://www.youtube.com/embed/LU9twc_-Dh8?si=SUSu1KYcR6U5msjm",
+  },
+];
