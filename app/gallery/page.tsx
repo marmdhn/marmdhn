051c7612f9302e2616ff6cfde6e8fd3507b98a5d
Ramadhan <makbarramadhan62@gmail.com>
2024-08-12 14:07:50 +0700

    refactor: add animation & loading in image gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -3,13 +3,12 @@
 import Image from "next/image";
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
+import { gallery } from "@/data/gallery";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 const Gallery = () => {
-  const images = [
-    { src: "/images/gallery/1.png", alt: "img1" },
-    { src: "/images/gallery/2.png", alt: "img2" },
-    { src: "/images/gallery/3.png", alt: "img3" },
-  ];
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   return (
     <div className="max-w-7xl px-8 xl:px-0 mx-auto py-10 h-full">
@@ -23,16 +22,57 @@ const Gallery = () => {
       </div>
 
       <div className="flex w-full flex-wrap gap-4 justify-center">
-        {images.map((image, index) => (
-          <Zoom zoomMargin={40} key={index}>
-            <Image
-              className="object-cover"
-              width={150}
-              height={150}
-              alt={image.alt}
-              src={image.src}
-            />
-          </Zoom>
+        {gallery.map((image, index) => (
+          <motion.div
+            key={index}
+            className="flex justify-center"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{
+              duration: 0.5,
+              ease: "easeOut",
+              delay: index * 0.1,
+            }}
+          >
+            <Zoom zoomMargin={40}>
+              <div className="relative">
+                {isLoading && (
+                  <div className="absolute inset-0 flex items-center justify-center bg-white">
+                    <svg
+                      className="animate-spin h-6 w-6 text-gray-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0a12 12 0 00-12 12h4zm8-8v4a4 4 0 01-4 4H4a8 8 0 008-8z"
+                      ></path>
+                    </svg>
+                  </div>
+                )}
+                <Image
+                  className="object-cover"
+                  width={300}
+                  height={300}
+                  alt={`img${index}`}
+                  src={`/images/gallery/${image.src}`}
+                  onLoadingComplete={() => setIsLoading(false)}
+                  onLoad={() => setIsLoading(false)}
+                />
+              </div>
+            </Zoom>
+          </motion.div>
         ))}
       </div>
     </div>
