40faa345a89c42dcc10859c492db23c7d7610459
Ramadhan <makbarramadhan62@gmail.com>
2024-08-12 20:31:42 +0700

    fix: bug zoom image

--- app/gallery/images.tsx
+++ app/gallery/images.tsx
@@ -9,32 +9,32 @@ const Images = ({ image, index }: { image: string; index: number }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   return (
-    <Zoom zoomMargin={40}>
-      <div className="relative">
-        {isLoading && (
-          <div className="absolute inset-0 flex items-center justify-center bg-white">
-            <svg
-              className="animate-spin h-6 w-6 text-gray-500"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0a12 12 0 00-12 12h4zm8-8v4a4 4 0 01-4 4H4a8 8 0 008-8z"
-              ></path>
-            </svg>
-          </div>
-        )}
+    <div className="relative">
+      {isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-white">
+          <svg
+            className="animate-spin h-6 w-6 text-gray-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0a12 12 0 00-12 12h4zm8-8v4a4 4 0 01-4 4H4a8 8 0 008-8z"
+            ></path>
+          </svg>
+        </div>
+      )}
+      <Zoom zoomMargin={40}>
         <Image
           className="object-cover"
           width={300}
@@ -44,8 +44,8 @@ const Images = ({ image, index }: { image: string; index: number }) => {
           onLoadingComplete={() => setIsLoading(false)}
           onLoad={() => setIsLoading(false)}
         />
-      </div>
-    </Zoom>
+      </Zoom>
+    </div>
   );
 };
 
