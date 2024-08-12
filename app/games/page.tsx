623251d09a2f160d06a60ee4db7e74483242af79
Ramadhan <makbarramadhan62@gmail.com>
2024-08-12 11:21:13 +0700

    refactor: adjust text

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -6,9 +6,7 @@ const GameList = () => {
     <div className="max-w-7xl px-8 xl:px-0 mx-auto py-10 h-full">
       <div className="text-center mb-8">
         <div className="flex flex-col gap-2">
-          <h1 className="text-4xl font-bold text-primary mt-4">
-            Games Section
-          </h1>
+          <h1 className="text-4xl font-bold text-primary mt-4">Games</h1>
           <p className="text-gray-700 dark:text-gray-300 text-lg font-semibold">
             Playing this games just for fun.
           </p>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,9 +5,9 @@ const Projects = () => {
     <div className="max-w-7xl px-8 xl:px-0 mx-auto py-10 h-full">
       <div className="text-center mb-8">
         <div className="flex flex-col gap-2">
-          <h1 className="text-4xl font-bold text-primary mt-4">Marmdhn</h1>
+          <h1 className="text-4xl font-bold text-primary mt-4">Project</h1>
           <p className="text-gray-700 dark:text-gray-300 text-lg font-semibold">
-            Personal Website Portfolio.
+            Previous projects I have worked on
           </p>
         </div>
       </div>
